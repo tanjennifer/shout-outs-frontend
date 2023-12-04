97a8dd7cf73f8f51df66228c4f3f2966ac9be47e
Jennifer <jennifertan1994@gmail.com>
2023-12-04 16:35:34 -0500

    saved header

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-// import { firebaseConfig } from "./firebaseConfig";
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+import { firebaseConfig } from "./firebaseConfig";
+// const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
