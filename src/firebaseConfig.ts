c664884d07a36e924c0083323d2179d5ac274fe6
Jennifer <jennifertan1994@gmail.com>
2023-12-04 16:38:24 -0500

    removed firebaseApp

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyACTLyLpPUSRtdXTOPnojgXIT3RFwBDafw",
   authDomain: "shoutouts-b2f72.firebaseapp.com",
@@ -6,3 +10,17 @@ export const firebaseConfig = {
   messagingSenderId: "1065973940417",
   appId: "1:1065973940417:web:4a457df6d6e182cc6fd440",
 };
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const authProvider = new GoogleAuthProvider();
+
+export function signInWithGoogle(): void {
+  signInWithPopup(auth, authProvider);
+}
+export function signOut(): void {
+  auth.signOut();
+}
+
+export const storage = getStorage(app);
