6fed55ec06e238975bd525441df516fde55f12d9
Jennifer <jennifertan1994@gmail.com>
2023-12-04 16:16:03 -0500

    removed

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { firebaseConfig } from "./firebaseConfig";
+// import { firebaseConfig } from "./firebaseConfig";
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyACTLyLpPUSRtdXTOPnojgXIT3RFwBDafw",
+  authDomain: "shoutouts-b2f72.firebaseapp.com",
+  projectId: "shoutouts-b2f72",
+  storageBucket: "shoutouts-b2f72.appspot.com",
+  messagingSenderId: "1065973940417",
+  appId: "1:1065973940417:web:4a457df6d6e182cc6fd440",
+};
